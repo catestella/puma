ad5940f73ab48d66bb3fbb8982d073c25c0a002a
Catherine-Rose Estella <estellacatherine@gmail.com>
2017-01-18 01:08:58 -0800

    Puma with JS code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 // product constructor
-function Shoes(name, size, price, image) {
+function Shoes(name, size, color, price, image) {
 	this.name = name
 	this.size = size
+	this.color = color
 	this.price = price
 	this.image = image
 }
 
 // create new products from the product constructor
-var clyde1 = new Shoes("CLYDE Dressed Part Deux FM", "size", "$100", "img/clyde1.jpg")
-var clyde2 = new Shoes("CLYDE Dressed Part Deux FM", "size", "$100", "img/clyde2.jpg")
-var clyde3 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde3.jpg")
-var clyde4 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde4.jpg")
-var clyde5 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde5.jpg")
-var clyde6 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde6.jpg")
-var clyde7 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde7.jpg")
-var clyde8 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde8.jpg")
-var clyde9 = new Shoes("Clyde Dressed Part Deux FM", "size", "$100", "img/clyde9.jpg")
+var clyde1 = new Shoes("CLYDE DRESSED", "size", "Black", "$100", "img/clyde1.jpg")
+var clyde2 = new Shoes("CLYDE DRESSED", "size", "White", "$100", "img/clyde2.jpg")
+var clyde3 = new Shoes("CLYDE SUITS", "size", "Oatmeal-Black-White", "$100", "img/clyde3.jpg")
+var clyde4 = new Shoes("CLYDE SUITS", "size", "Solar Power-Black-White", "$100", "img/clyde4.jpg")
+var clyde5 = new Shoes("CLYDE NYC", "size", "Vibrant Orange-Royal", "$85", "img/clyde5.jpg")
+var clyde6 = new Shoes("CLYDE WRAITH", "size", "Cordovan-Corovan", "$79.99", "img/clyde6.jpg")
+var clyde7 = new Shoes("CLYDE WRAITH", "size", "Black-Black", "$79.99", "img/clyde7.jpg")
+var clyde8 = new Shoes("CLYDE B&C", "size", "Steel Gray-Peacoat", "$85", "img/clyde8.jpg")
+var clyde9 = new Shoes("CLYDE B&C", "size", "Winetasting-Black", "$85", "img/clyde9.jpg")
 
 // create an array to add products to
 var shoesArray = new Array
@@ -30,12 +31,14 @@ for(var i = 0; i < shoesArray.length; i++) {
 	var newDiv  = document.createElement("div")
 	var nameH1  = document.createElement("h1")
 	var sizeH4  = document.createElement("h4")
+	var colorH4 = document.createElement("h4")
 	var btn     = document.createElement("button")
 	var image   = document.createElement("img")
 
 	// create text for new elements
 	var tName   = document.createTextNode(shoesArray[i].name)
 	var tSize   = document.createTextNode("Available size: " + shoesArray[i].size)
+	var tColor = document.createTextNode("Color: " + shoesArray[i].color)
 	var tButton = document.createTextNode("Buy Now!")
 
 	// update source attributes
@@ -50,16 +53,14 @@ for(var i = 0; i < shoesArray.length; i++) {
 	// add text to elements
 	nameH1.appendChild(tName)
 	sizeH4.appendChild(tSize)
-	// colorH4.appendChild(tColor)
-	// stockH4.appendChild(tStock)
+	colorH4.appendChild(tColor)
 	btn.appendChild(tButton)
 
 	// add elements to new div
 	newDiv.appendChild(nameH1)
 	newDiv.appendChild(image)
 	newDiv.appendChild(sizeH4)
-	// newDiv.appendChild(colorH4)
-	// newDiv.appendChild(stockH4)
+	newDiv.appendChild(colorH4)
 	newDiv.appendChild(btn)
 
 	// add new div to new item div
